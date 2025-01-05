4b85c65c24130eb7fb654eb46e9e7ebe48792b6b
sheethal-11 <sheethal.gopa@gmail>
2025-01-05 17:59:00 +0000

    java-file

---
+++ Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+	public static void main(String [] args) {
+		int rollno = 1773;
+		String sname = "sheethal";
+		String course = "python";
+		System.out.println( "LEARN AND LEAD");
+		System.out.println("my name is : ", + sname);
+		System.out.println("my course is : ", + course);
+		System.out.println("my roll number is : ", + rollno);
+	}
+}
