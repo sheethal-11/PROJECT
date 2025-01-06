c0f20cfbeaed5d6d96348fda33d3bdcae40c0815
sheethal-11 <143023532+sheethal-11@users.noreply.github.com>
2025-01-06 13:21:28 +0530

    Updated

--- Demo.java
+++ Demo.java
@@ -4,9 +4,9 @@ public class Demo {
 		String sname = "sheethal";
 		String course = "python";
 		System.out.println( "LEARN AND LEAD");
-		System.out.println("my name is : ", + sname);
-		System.out.println("my course is : ", + course);
-		System.out.println("my roll number is : ", + rollno);
+		System.out.println("my name is : " + sname);
+		System.out.println("my course is : " + course);
+		System.out.println("my roll number is : " + rollno);
 		System.out.println("task done");
 	}
 }
